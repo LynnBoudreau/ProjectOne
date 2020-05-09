015438ff8175c3138ae27db3575d375527800ea8
Karen Astell <KarenAstell@gmail.com>
2020-05-08 20:57:55 -0400

    if underage, hide modal pieces and show new ones, button will direct to websters dictionary definition of underage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,20 @@
-$(".check-age").on("click", function () {
+$(".modal").addClass("is-active");
+$(".grounded").hide();
+$(".age-check").on("click", ageCheck);
+
+function ageCheck() {
   var month = $(".month").val();
   var day = $(".day").val();
   var year = $(".year").val();
   var userDOB = month + " " + day + " " + year;
   console.log(userDOB);
-  var ageCheck = moment().diff(userDOB, "years");
-});
+  var ageChecker = moment().diff(userDOB, "years");
+  console.log(ageChecker);
+  if (ageChecker >= 21) {
+    $(".age-check").attr("href", "index.html");
+    $(".modal").removeClass("is-active");
+  } else {
+    $(".dob-check").hide();
+    $(".grounded").show();
+  }
+}
