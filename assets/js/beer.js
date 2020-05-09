40399bb77ac6d148f3548b112363ce59b277e2ea
John Bouchard <jbouchard13@gmail.com>
2020-05-09 10:33:13 -0400

    started adding js from test file, still need class/id names

--- assets/js/beer.js
+++ assets/js/beer.js
@@ -1,1 +1,20 @@
+var googleApiKey = "AIzaSyADK5Lk6M5yKgWBra5haAe7z-e_l-PlFdE";
 
+var breweryUrl = `https://api.openbrewerydb.org/breweries/?by_city=${cityInput}&by_state=${stateInput}&per_page=5`;
+
+// Have an empty array to push the coordinates to from the ajax call
+var lonLatLocation = [];
+
+// Create a button and event listener for that button
+$("").on("click", function (e) {
+  e.preventDefault();
+
+  // Grab input from the city and state input boxes
+  var cityInput = $("").val();
+  var stateInput = $("").val();
+
+
+
+    }
+  });
+});
